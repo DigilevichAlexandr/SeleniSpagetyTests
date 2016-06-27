01a383b6914af937ef522010e5ec3e62850f75c3
Alexandr Digilevich <Alexandr_Digilevich@epam.com>
2016-06-27 16:54:49 +0300

    refactoring: roundbuttonpagemodel, composepagemodel, boxpagemodel, inboxpagemodel

---
+++ SeleniumSpagetyCode/AutomationTests/PageModels/Box/ComposePageModel.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace AutomationTests.PageModels
+{
+    public class ComposePageModel : PageModel
+    {
+        [FindsBy(How = How.XPath, Using = "")]
+        public IWebElement To { get; set; }
+        [FindsBy(How = How.XPath, Using = "//input[@name='subjectbox']")]
+        public IWebElement Subject { get; set; }
+        [FindsBy(How = How.XPath, Using = "//div[@role='textbox']")]
+        public IWebElement MessageBody { get; set; }
+        [FindsBy(How = How.XPath, Using = "//div[text()='Send']")]
+        public IWebElement SendButton { get; set; }
+    }
+}
